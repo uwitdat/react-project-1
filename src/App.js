9d0cb3ae2d6f5bc4967555eaa96443b8f985a71a
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-07 16:13:44 -0400

    completedProject

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [suggestions, setSuggestions] = useState([])
+  const [users, setUsers] = useState([])
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('https://reqres.in/api/users')
+      const users = res.data.data
+      setUsers(users)
+    }
+    fetchData()
+  }, [])
+
+  const onSuggestHangler = (text) => {
+    setText(text)
+    setSuggestions([])
+  }
+
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(usr => {
+        const regex = new RegExp(`${text}`, "gi")
+        return usr.email.match(regex)
+      })
+    }
+    setSuggestions(matches)
+    setText(text)
+  }
+
   return (
     <div className="App">
-
+      <input type="text" className='col-md-10 input' style={{ marginTop: '3rem' }}
+        onChange={(e) => onChangeHandler(e.target.value)}
+        value={text}
+        onBlur={() => {
+          setTimeout(() => {
+            setSuggestions([])
+          }, 100)
+        }}
+      />
+      {suggestions && suggestions.map((sug, i) => (
+        <div key={i} className='suggestion col-md-10 justify-content-md-center'
+          onClick={() => onSuggestHangler(sug.email)}
+        >{sug.email}
+        </div>
+      ))}
     </div>
   );
 }
